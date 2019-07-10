c5bf935cab35b881a6657e16ad07cebae973bfde
Sam Zhao <zhao.shengjun@gmail.com>
2019-07-10 21:50:50 +1000

    event list

--- gatsby-theme-events/src/components/event-list.js
+++ gatsby-theme-events/src/components/event-list.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "gatsby";
 
-const EventList = ({ events }) => <pre>{JSON.stringify(events)}</pre>;
+const EventList = ({ events }) => {
+  return (
+    <>
+      <h1>Upcoming events</h1>
+      <ul>
+        {events.map(event => (
+          <li key={event.id}>
+            <strong>
+              <Link to={event.slug}> {event.name}</Link>
+            </strong>
+            <p>
+              {new Date(event.startDate).toLocaleDateString()} in{" "}
+              {event.location}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export { EventList };
